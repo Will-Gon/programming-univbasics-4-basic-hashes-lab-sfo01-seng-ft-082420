495d3e33caef49bf03d572a313efebf8ed3c4104
Will Gonzalez <will.colocho@gmail.com>
2020-08-07 07:38:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  # return an empty hash
+  {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  {:name => "Will", :hobby => "Football"
 end
 
 def pioneer
